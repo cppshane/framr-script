92249f976188ec5ebf7bc61816d8994e0948edc3
Shane Duffy <shane@shaneduffy.io>
2022-07-30 14:43:28 -0700

    init commit

---
+++ frame.sh
@@ -0,0 +1,14 @@
+# Create inner clip
+innerClip="inner_$1"
+ffmpeg -i $1 -vf scale=1800:1012 -preset slow -crf 18 $innerClip -y
+
+# Create outer clip
+outerClip="outer_$1"
+ffmpeg -i $1 -vf "boxblur=30" -c:a copy $outerClip -y
+
+# Apply shadow to outer clip
+shadowClip="shadow_$1"
+ffmpeg -i $outerClip -i shadow.png -filter_complex "overlay" $shadowClip -y
+
+# Apply inner clip to shadow clip
+ffmpeg -i $shadowClip -i $innerClip -filter_complex "overlay = 60:34" result.mp4 -y
